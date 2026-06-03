5da490f4b44562ed291091f160b1e5e0acef94f9
Ahmad Fikri Afandi <afandiahmadfikri@gmail.com>
2026-06-03 11:15:01 +0000

    Fix Cloud Run production startup

--- server.ts
+++ server.ts
@@ -530,6 +530,12 @@ export default app;
 
 if (process.env.NODE_ENV !== 'production') {
   startViteMiddleware().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
+} else {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
